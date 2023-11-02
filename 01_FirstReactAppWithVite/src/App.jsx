e4e96625a4c92bba3dd04647d90ca0babbc8d76b
Deepak-200259 <deepwarriors2002@gmail.com>
2023-11-02 12:32:29 +0530

    Basic React Apps with and Without Vite

---
+++ 01_FirstReactAppWithVite/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+	return <h1>React with Vite | Deepak Saini</h1>;
+}
+
+export default App;
---
+++ 01_basic-react-app-without-vite/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+	return <h1>Learning React Without Vite | Deepak Saini</h1>;
+}
+
+export default App;
